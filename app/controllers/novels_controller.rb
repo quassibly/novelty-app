d83c6209b9b6a380dc5d99a6e268cc904ec05f7d
Daniel Blake <daniel.blake1@hotmail.co.uk>
2018-05-29 12:36:54 +0200

    Controller start

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -1,24 +1,49 @@
 class NovelsController < ApplicationController
 
   def new
-
+    @novel = Novel.new
     authorize @novel
   end
 
   def create
+    @novel = Novel.new(novel_params)
+    @novel.user_id = current_user.id
+    @novel.created_at = Time.now
+    @novel.updated_at = Time.now
     authorize @novel
+    if @novel.save!
+      redirect_to edit_novel_path(@novel)
+    else
+      render :new
+    end
   end
 
   def edit
+    @novel = Novel.find(params[:id])
+
     authorize @novel
   end
 
   def update
+    @novel = Novel.find(params[:id])
+    @novel.updated_at = Time.now
     authorize @novel
+    if @novel.update!
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @novel = Novel.find(params[:id])
     authorize @novel
+    @novel.destroy
   end
 
+  private
+
+  def novel_params
+    params.require(:novel).permit(:title)
+  end
 end
