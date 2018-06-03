9ce6b9fd7231469c865c4633bcc8c45be747bfec
Lota Plezere <lota.plezere@gmail.com>
2018-06-03 19:18:39 +0200

    flashes styling and disappearance

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,5 @@
 
+import { removeFlashes } from 'packs/flashes';
+
+
+removeFlashes();
---
+++ app/javascript/packs/flashes.js
@@ -0,0 +1,14 @@
+function removeFlashes() {
+  const flash = document.querySelector(".alert");
+  if (document.body.contains(flash)) {
+    if (flash.classList.contains("alert-info")) {
+      setTimeout(function(){ flash.remove(); }, 2900);
+    }
+    const closeBtn = document.querySelector(".close-flash");
+    const closeFlash = closeBtn.addEventListener("click", (event) => {
+      closeBtn.parentNode.remove();
+    })
+  }
+}
+
+export { removeFlashes }
