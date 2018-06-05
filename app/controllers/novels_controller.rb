fb3aa227817d4ba4f4813a2e92c7e2d2f20e5497
Kolja Lucht <kolja.lucht@gmail.com>
2018-06-05 14:57:16 +0200

    Added todays word count to stats

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -47,6 +47,7 @@ class NovelsController < ApplicationController
   def edit
     @sentences = Sentence.all
     @novel = Novel.find(params[:id])
+    @yesterday_total_all = yesterday_total_all
     @session = WritingSession.create(created_at: Time.now, user: current_user, novel: @novel)
     if @novel.nil?
       self.new
@@ -91,6 +92,10 @@ class NovelsController < ApplicationController
 
   def yesterday_total
     @yesterday_total = WritingSession.where("created_at >= ?", @novel.goal_start_date).where("created_at < ?", Time.now.to_date).sum(:session_wordcount)
+  end
+
+  def yesterday_total_all
+    @yesterday_total_all = WritingSession.where("created_at < ?", Time.now.to_date).sum(:session_wordcount)
   end
 
   def days_left
