90e0166cc1a02719d14432c940a75ed4e14041ce
Kolja Lucht <kolja.lucht@gmail.com>
2018-06-05 17:09:38 +0200

    implemented progress bar on edit page

---
+++ app/javascript/packs/progress_bar.js
@@ -0,0 +1,31 @@
+const wordGoal = document.getElementById("word-goal").innerHTML;
+const textarea = document.querySelector("#novel-text-area")
+
+function count() {
+  const textareaVal = textarea.value.trim()
+
+  // replace all whitespace, enters, breaks with a simple space
+  let words = textareaVal.replace(/\s+/gi, ' ').split(' ').length;
+  const chars = textareaVal.length;
+  let progress = (words / wordGoal) * 100;
+  const progressBar = document.querySelector("#progress")
+  const counter = document.querySelector("#counter")
+  console.log(words);
+  console.log(textareaVal);
+  if(!chars)words=0;
+  if(!chars)progress=0;
+
+
+  // do not let width be more than 100%;
+  if (progress > 100) {
+    progress = 100;
+  }
+  progressBar.style.width = `${progress}%`;
+  counter.innerHTML = words;
+}
+
+
+const novelTextArea = document.getElementById("novel-text-area")
+novelTextArea.addEventListener("input", () => { count(); })
+document.addEventListener('DOMContentLoaded', count)
+
