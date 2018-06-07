b6c970817474380edd37f39be2c4689b83ce09a8
Meredith Avery <quassibly@gmail.com>
2018-06-07 18:42:06 +0200

    meta revised again

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -2,15 +2,15 @@
 
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["Novelty ... keep writing all the way to The End"]
+    content_for?(:meta_title) ? content_for(:meta_title) : "Novelty ... keep writing all the way to The End"
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["The app designed to help you with NaNoWriMo from beginning to The End"]
+    content_for?(:meta_description) ? content_for(:meta_description) : "The app designed to help you with NaNoWriMo from beginning to The End"
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : "og-image.png")
     # little twist to make it work equally with an asset or a url
     meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
